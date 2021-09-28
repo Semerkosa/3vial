63c7b68b7c42afa9e86897d3ba5bcfb39d559ddb
stspot <zstefchev@yahoo.com>
2021-09-28 09:24:10 +0200

    accounts-microservice-jointrivial

---
+++ accounts-microservice-jointrivial/src/main/java/accounts/AccountsMicroserviceJointrivialApplication.java
@@ -0,0 +1,13 @@
+package accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsMicroserviceJointrivialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsMicroserviceJointrivialApplication.class, args);
+	}
+
+}
---
+++ accounts-microservice-jointrivial/src/test/java/accounts/AccountsMicroserviceJointrivialApplicationTests.java
@@ -0,0 +1,13 @@
+package accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsMicroserviceJointrivialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
