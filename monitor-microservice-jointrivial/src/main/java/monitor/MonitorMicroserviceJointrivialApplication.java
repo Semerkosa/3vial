7c6cc26cfcd862d4c904cd8e2dab167375e059f0
stspot <zstefchev@yahoo.com>
2021-09-28 14:57:30 +0200

    Creating a microservice - Monitor

---
+++ monitor-microservice-jointrivial/src/main/java/monitor/MonitorMicroserviceJointrivialApplication.java
@@ -0,0 +1,13 @@
+package monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorMicroserviceJointrivialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorMicroserviceJointrivialApplication.class, args);
+	}
+
+}
