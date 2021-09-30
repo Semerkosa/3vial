4668101b801d12a380e8500c78efe0f676c19f95
GGerginov <45296057+GGerginov@users.noreply.github.com>
2021-09-30 20:42:42 +0300

    Creating a microservice - Reference

---
+++ reference/src/main/java/trivial/ReferenceMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package trivial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReferenceMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReferenceMicroserviceApplication.class, args);
+    }
+
+}
---
+++ reference/src/test/java/trivial/ReferenceMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package trivial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReferenceMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
