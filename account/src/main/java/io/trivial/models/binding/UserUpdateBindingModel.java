4ebbf8951124d5340e0e4163f90d5f527a14422e
stspot <zstefchev@yahoo.com>
2021-10-02 22:36:56 +0200

    New models

--- account/src/main/java/io/trivial/models/binding/UserUpdateBindingModel.java
+++ account/src/main/java/io/trivial/models/binding/UserUpdateBindingModel.java
@@ -1,4 +1,28 @@
 package io.trivial.models.binding;
 
+import io.trivial.models.entites.Address;
+
 public class UserUpdateBindingModel {
+
+    private String password;
+    private AddressBindingModel address;
+
+    public UserUpdateBindingModel() {
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public AddressBindingModel getAddress() {
+        return address;
+    }
+
+    public void setAddress(AddressBindingModel address) {
+        this.address = address;
+    }
 }
