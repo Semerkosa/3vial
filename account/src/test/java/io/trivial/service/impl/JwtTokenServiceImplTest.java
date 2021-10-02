9bc36fe49c6b48f0a5d511c47a89245548878fea
stspot <zstefchev@yahoo.com>
2021-10-02 22:51:19 +0200

    Add test class for JwtTokenServiceImpl.

---
+++ account/src/test/java/io/trivial/service/impl/JwtTokenServiceImplTest.java
@@ -0,0 +1,12 @@
+package io.trivial.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JwtTokenServiceImplTest {
+
+    @Test
+    void createNewToken() {
+    }
+}
