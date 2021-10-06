d3f6d721b47583e870733c9ba47148b2796c78c7
stspot <zstefchev@yahoo.com>
2021-10-06 12:07:54 +0200

    Add binding models.

---
+++ account/src/main/java/io/trivial/models/binding/BaseBindingModel.java
@@ -0,0 +1,16 @@
+package io.trivial.models.binding;
+
+public class BaseBindingModel {
+        private String id;
+
+        public BaseBindingModel() {
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+}
