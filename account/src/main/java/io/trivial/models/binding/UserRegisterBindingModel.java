55962d1f234125040de4b3a727385d4852ed7a47
stspot <zstefchev@yahoo.com>
2021-10-06 12:12:54 +0200

    Add content to UserRegisterBindingModel.

--- account/src/main/java/io/trivial/models/binding/UserRegisterBindingModel.java
+++ account/src/main/java/io/trivial/models/binding/UserRegisterBindingModel.java
@@ -1,4 +1,36 @@
 package io.trivial.models.binding;
 
 public class UserRegisterBindingModel {
+
+    private String email;
+    private String password;
+    private AddressBindingModel address;
+
+    public UserRegisterBindingModel() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public AddressBindingModel getAddress() {
+        return address;
+    }
+
+    public void setAddress(AddressBindingModel address) {
+        this.address = address;
+    }
+
 }
