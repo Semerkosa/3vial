1728c1bcdaeaa5f1b64410a769a4195d397eef65
Semerkosa <60141925+Semerkosa@users.noreply.github.com>
2021-10-08 13:32:43 +0300

    Adds configuration
    Also adds new dependancies

---
+++ asset/src/main/java/com/jointrivial/asset/Nordigen/config/ApplicationBeanConfiguration.java
@@ -0,0 +1,22 @@
+package com.jointrivial.asset.Nordigen.config;
+
+import com.google.gson.*;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.*;
+
+@Configuration
+public class ApplicationBeanConfiguration {
+
+    @Bean
+    public Gson gson() {
+        return new GsonBuilder()
+                .excludeFieldsWithoutExposeAnnotation()
+                .setPrettyPrinting()
+                .create();
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
---
+++ asset/src/main/java/com/jointrivial/asset/Nordigen/models/services/BankServiceModel.java
@@ -0,0 +1,82 @@
+package com.jointrivial.asset.Nordigen.models.services;
+
+import java.util.List;
+
+// ASPSP = Account Servicing Payment Service Provider(e.g., bank...)
+public class BankServiceModel {
+
+    private String id;
+    private String name;
+    private String bic;
+    private String transactionTotalDays;
+    private List<String> countries;
+    private String logo;
+
+    public BankServiceModel() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public BankServiceModel setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BankServiceModel setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getBic() {
+        return bic;
+    }
+
+    public BankServiceModel setBic(String bic) {
+        this.bic = bic;
+        return this;
+    }
+
+    public String getTransactionTotalDays() {
+        return transactionTotalDays;
+    }
+
+    public BankServiceModel setTransactionTotalDays(String transactionTotalDays) {
+        this.transactionTotalDays = transactionTotalDays;
+        return this;
+    }
+
+    public List<String> getCountries() {
+        return countries;
+    }
+
+    public BankServiceModel setCountries(List<String> countries) {
+        this.countries = countries;
+        return this;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public BankServiceModel setLogo(String logo) {
+        this.logo = logo;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "BankServiceModel --> " +
+                "id = " + id +
+                ", name = " + name +
+                ", bic = " + bic +
+                ", transactionTotalDays = " + transactionTotalDays +
+                ", countries = " + countries +
+                ", logo = " + logo;
+    }
+}
