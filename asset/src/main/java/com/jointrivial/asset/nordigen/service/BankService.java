588dcfea0b74d93d00c86da4ebf2a046af236c71
TSemerdzhiev <60141925+Semerkosa@users.noreply.github.com>
2021-10-11 13:20:07 +0300

    Deletes duplicated file

--- asset/src/main/java/com/jointrivial/asset/nordigen/service/BankService.java
+++
@@ -1,9 +0,0 @@
-package com.jointrivial.asset.nordigen.service;
-
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.List;
-
-public interface BankService {
-    List<String> allBankNamesInCountry(String country) throws IOException, URISyntaxException, InterruptedException;
-}
