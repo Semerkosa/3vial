79fc1466ca472b659050794f3075e5c2ceef792d
plm012 <plm012@abv.bg>
2021-10-12 11:38:43 +0300

    Header for the landing page

--- landingPage/client/src/App.js
+++ landingPage/client/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './Logo-1.png';
+import group51 from './Group 51.png';
+import group318 from './Group 318.png';
+import group319 from './Group 319.png';
+import vector from './Vector.png';
+import facebook from './Social Icons FB.png';
+import instagram from './Social Icons IG.png';
+import twitter from './Social Icons TW.png';
+import './reset.css';
+import './style.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { NavLink } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+    </>
   );
+}
+class Header extends React.Component {
+  render() {
+    return (
+      <>
+        <header>
+          <img id="logo" src={logo} alt="logo" />
+          <BrowserRouter>
+            <nav className="nav">
+              <NavLink exact id='home' to='/' activeClassName="active"> Home</NavLink>
+              <NavLink id='features' to='/features'  >Features</NavLink>
+              <NavLink id='contact' to='/contact'  >Contact</NavLink>
+            </nav>
+          </BrowserRouter>
+          <button>Get early access</button>
+        </header>
+        <hr />
+      </>);
+  }
 }
 
 export default App;
