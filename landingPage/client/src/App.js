8359d6d2d31e1bd09a994ada222a7f81e315fb1c
plm012 <69417020+plm012@users.noreply.github.com>
2021-10-14 13:53:42 +0300

    Footer for the landing page (#90)
    * Header for the landing page
    
    * Sections for the landing page
    
    * Revert "Sections for the landing page"
    
    This reverts commit ca69f234cc5d4d7a5a8f4c664a38825ff27a5a62.
    
    * Sections for the landing page
    
    * Footer for the landing page
    
    * Footer for the landing page

--- landingPage/client/src/App.js
+++ landingPage/client/src/App.js
@@ -18,13 +18,16 @@ import { BrowserRouter } from 'react-router-dom';
 function App() {
   return (
     <>
-      <Header />
-      <Section1 />
-      <Section2 />
-      <Section3 />
-      <Section2 />
-      <Section3 />
-      <Section6 />
+      <main>
+        <Header />
+        <Section1 />
+        <Section2 />
+        <Section3 />
+        <Section2 />
+        <Section3 />
+        <Section6 />
+      </main>
+      <Footer />
     </>
   );
 }
@@ -153,4 +156,25 @@ class Section6 extends React.Component {
       </>);
   }
 }
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer id='footer'>
+        <div className="footer-container">
+          <p>
+            © 2021 Trivial. All rights reserved
+          </p>
+          <div className="icons">
+            <a href="https://www.instagram.com/"><img src={instagram} alt='IG' /></a>
+            <a href="https://twitter.com/"><img src={twitter} alt='TW' /></a>
+            <a href="https://www.facebook.com/"><img src={facebook} alt='FB' /></a>
+          </div>
+        </div>
+      </footer>
+
+    );
+  }
+}
+
 export default App;
