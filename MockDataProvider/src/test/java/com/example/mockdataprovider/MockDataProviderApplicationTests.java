040bc09cb2b85a149cffe851395e2009d2d02793
Semerkosa <60141925+Semerkosa@users.noreply.github.com>
2021-10-19 14:23:35 +0300

    Create initial project

---
+++ MockDataProvider/src/test/java/com/example/mockdataprovider/MockDataProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mockdataprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockDataProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
