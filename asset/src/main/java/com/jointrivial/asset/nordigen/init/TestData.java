bb35d65cb37d869f78753110608bd05a73a42f74
Semerkosa <60141925+Semerkosa@users.noreply.github.com>
2021-10-21 19:55:02 +0300

    Finished user-bank linking methods
    Anyone can test with the Init.TestData.class

---
+++ asset/src/main/java/com/jointrivial/asset/nordigen/init/TestData.java
@@ -0,0 +1,59 @@
+package com.jointrivial.asset.nordigen.init;
+
+import com.jointrivial.asset.nordigen.api.NordigenAccountInfoAPI;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+@Component
+public class TestData implements CommandLineRunner {
+
+    private final NordigenAccountInfoAPI api;
+
+    public TestData(NordigenAccountInfoAPI api) {
+        this.api = api;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        printSteps();
+
+        // FIRST METHOD
+//        System.out.println(api.createRequisition("Pesho", "PeshoReference", "https://assets.sainsburys-groceries.co.uk/gol/7866708/1/640x640.jpg", new ArrayList<>()));
+
+        // SECOND METHOD
+//        System.out.println(api.createBankAuthorizationLinkForRequisition("", "HSBC_HBUKGB4B"));
+
+        // THIRD METHOD
+//        System.out.println(api.getRequisitionById(""));
+    }
+
+    private void printSteps() {
+        System.err.println("---------------------------------");
+        System.err.println("1) RUN ONLY THE FIRST METHOD");
+        System.err.println("2) SAVE THE ID  --->  PASS AS PARAMETER TO OTHER METHODS");
+        System.err.println("3) RUN ONLY THE SECOND METHOD");
+        System.err.println("4) FOLLOW THE LINK TO AUTHORISE");
+        System.err.println("5) RUN ONLY THE THIRD METHOD");
+        System.err.println("If you need to RE-DO, just change the 'reference_id' in the first method to a random value");
+        System.err.printf("---------------------------------%n%n");
+    }
+
+    private void testMethods() {
+//        System.out.println(api.getAllBanksForCountry("bg"));
+//
+//
+//        System.out.println(api.createEndUserAgreement("pesho", "FIBANK_FINVBGSF", 50));
+//        System.out.println(api.getEndUserAgreementById(""));
+//        System.out.println(api.getAllEndUserAgreementsForUserByUserId("pesho"));
+//        api.deleteEndUserAgreementById("");
+//
+//
+//        System.out.println(api.createRequisition("pesho", "peshoRef", "https://assets.sainsburys-groceries.co.uk/gol/7866708/1/640x640.jpg", new ArrayList<>()));
+//        System.out.println(api.getAllRequisitions());
+//        System.out.println(api.createBankAuthorizationLinkForRequisition("4c8bf6ac-5e3d-4ba2-9e1c-6b8d1e2e51d2", "FIBANK_FINVBGSF"));
+//        System.out.println(api.getRequisitionById(""));
+//        api.deleteRequisitionById("");
+    }
+}
