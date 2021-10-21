b34d49c95b0797b4a71dbc9903e754f9265fa0a5
stspot <zstefchev@yahoo.com>
2021-10-21 20:34:37 +0200

    Add a default user.

--- account/src/main/java/io/trivial/utils/Loader.java
+++ account/src/main/java/io/trivial/utils/Loader.java
@@ -4,8 +4,12 @@ import io.trivial.enums.PrivilegeEnum;
 import io.trivial.enums.RoleEnum;
 import io.trivial.models.entites.Privilege;
 import io.trivial.models.entites.Role;
+import io.trivial.models.service.AddressServiceModel;
+import io.trivial.models.service.UserServiceModel;
 import io.trivial.repositories.PrivilegeRepository;
 import io.trivial.repositories.RoleRepository;
+import io.trivial.repositories.UserRepository;
+import io.trivial.service.UserService;
 
 import java.util.ArrayList;
 
@@ -19,15 +23,32 @@ public class Loader implements CommandLineRunner {
 
     private final PrivilegeRepository privilegeRepository;
     private final RoleRepository roleRepository;
+    private final UserRepository userRepository;
+    private final UserService userService;
 
     @Autowired
-    public Loader(PrivilegeRepository privilegeRepository, RoleRepository roleRepository) {
+    public Loader(PrivilegeRepository privilegeRepository, RoleRepository roleRepository,
+    		UserRepository userRepository, UserService userService) {
         this.privilegeRepository = privilegeRepository;
         this.roleRepository = roleRepository;
+        this.userRepository = userRepository;
+		this.userService = userService;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        //TODO
+        if(this.userRepository.count() == 0) {
+        	UserServiceModel user = new UserServiceModel();
+        	user.setEmail("ivan@example.com");
+        	user.setPassword("123456");
+        	AddressServiceModel address = new AddressServiceModel();
+        	address.setCity("Sofia");
+        	address.setCountry("Bulgaria");
+        	address.setPostCode("1000");
+        	address.setStreet("Vitoshka");
+        	address.setStreetNumber("100");
+        	user.setAddress(address);
+        	this.userService.register(user);
+        }
     }
 }
