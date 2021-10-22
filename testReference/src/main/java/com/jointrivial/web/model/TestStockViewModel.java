4ca59725ae4a813704850b68d981a926c215cbf0
GGerginov <45296057+GGerginov@users.noreply.github.com>
2021-10-22 20:58:12 +0300

    Basic functionality

---
+++ testReference/src/main/java/com/jointrivial/web/model/TestStockViewModel.java
@@ -0,0 +1,28 @@
+package com.jointrivial.web.model;
+
+public class TestStockViewModel {
+
+    private String symbol;
+    private Double price;
+
+    public TestStockViewModel(String symbol, Double price) {
+        this.symbol = symbol;
+        this.price = price;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
