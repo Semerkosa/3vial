7d2c20baceffe4bdcbf05a459d13844f5313bdec
Semerkosa <60141925+Semerkosa@users.noreply.github.com>
2021-10-26 11:15:32 +0300

    added .gitignore and some changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BalancesButton />
   );
+}
+
+class BalancesButton extends React.Component {
+  render() {
+    return (
+      <button>Get balances</button>
+    );
+  }
 }
 
 export default App;
