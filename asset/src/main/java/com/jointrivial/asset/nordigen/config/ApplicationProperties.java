089527d52f98d95f018e670a16f4af841b1b7fa6
Semerkosa <60141925+Semerkosa@users.noreply.github.com>
2021-10-26 14:02:21 +0300

    added url as argument

--- asset/src/main/java/com/jointrivial/asset/nordigen/config/ApplicationProperties.java
+++ asset/src/main/java/com/jointrivial/asset/nordigen/config/ApplicationProperties.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "api")
+@ConfigurationProperties(prefix = "arguments")
 public class ApplicationProperties {
 
     private String baseUrl;
