e1f6939396ae716c3520f6b5d6ceed2ed3d9f0d0
Semerkosa <60141925+Semerkosa@users.noreply.github.com>
2021-10-30 16:13:29 +0300

    changes package name
    orgKeys becomes keysOrg

--- asset/src/main/java/com/jointrivial/asset/nordigen/models/services/keysOrganization/KeyOrganizationServiceModel.java
+++ asset/src/main/java/com/jointrivial/asset/nordigen/models/services/keysOrganization/KeyOrganizationServiceModel.java
@@ -1,4 +1,4 @@
-package com.jointrivial.asset.nordigen.models.services.organizationKeys;
+package com.jointrivial.asset.nordigen.models.services.keysOrganization;
 
 import com.google.gson.annotations.Expose;
 
