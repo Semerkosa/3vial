5d098e65d1f6fd65f908d84ff3d66ec00e0a3e1c
plm012 <69417020+plm012@users.noreply.github.com>
2021-10-30 15:45:16 +0300

    Add security to the landing page (#136)

---
+++ landingPage/server/src/main/java/com/jointrivial/server/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.jointrivial.server.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final PasswordEncoder passwordEncoder;
+    @Value("${username}")
+    private String username;
+    @Value("${password}")
+    private String password;
+    @Value("${ip}")
+    private String allowedIPAddress;
+
+    @Autowired
+    public SecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser(username).password(passwordEncoder.encode(password)).roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/get_all").fullyAuthenticated()
+                .antMatchers("/count").hasIpAddress(allowedIPAddress)
+                .antMatchers("/sign_up").hasIpAddress(allowedIPAddress)
+               .antMatchers("/login").permitAll()
+              .anyRequest().authenticated().and()
+                .formLogin()
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/home", true)
+                .failureUrl("/login.html?error=true")
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .deleteCookies("JSESSIONID");
+    }
+}
