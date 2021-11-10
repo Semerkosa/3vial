d4824411361886cd12393f2737f5257362d84c8f
ANeshkova <anitowurth@abv.bg>
2021-11-10 12:11:32 +0200

    Source Manager: Create database model
    #207

---
+++ source-manager/src/main/java/com/jointrivial/sourcemanager/model/entity/SourceIdentifier.java
@@ -0,0 +1,38 @@
+package com.jointrivial.sourcemanager.model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "source_manager")
+public class SourceIdentifier {
+
+    private String referenceId;
+    private String requisitionId;
+
+    public SourceIdentifier() {
+    }
+
+    @Id
+    @Column(name = "reference_id", nullable = false, unique = true)
+    public String getReferenceId() {
+        return referenceId;
+    }
+
+    public SourceIdentifier setReferenceId(String referenceId) {
+        this.referenceId = referenceId;
+        return this;
+    }
+
+    @Column(name = "requisition_id")
+    public String getRequisitionId() {
+        return requisitionId;
+    }
+
+    public SourceIdentifier setRequisitionId(String requisitionId) {
+        this.requisitionId = requisitionId;
+        return this;
+    }
+}
