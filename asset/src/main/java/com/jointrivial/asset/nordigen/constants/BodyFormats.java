2c1cf5be71515f33ed01b11931292d184a9b23e9
Semerkosa <60141925+Semerkosa@users.noreply.github.com>
2021-11-11 15:31:56 +0200

    new token logic added

--- asset/src/main/java/com/jointrivial/asset/nordigen/constants/BodyFormats.java
+++
@@ -1,7 +0,0 @@
-package com.jointrivial.asset.nordigen.constants;
-
-public class BodyFormats {
-
-    public static final String URLENCODED_BODY_FORMAT = "application/x-www-form-urlencoded";
-    public static final String JSON_BODY_FORMAT = "application/json";
-}
