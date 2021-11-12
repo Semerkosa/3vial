f18b695e42162114495129d463b7c762e06b0db6
Borislav Nenov <borislav.nenov.93@gmail.com>
2021-11-12 12:30:32 +0200

    Removed unnecessary account methods from nordigen api class

--- source-manager/src/main/java/com/jointrivial/sourcemanager/nordigen/api/NordigenAccountInfoAPI.java
+++ source-manager/src/main/java/com/jointrivial/sourcemanager/nordigen/api/NordigenAccountInfoAPI.java
@@ -101,22 +101,6 @@ public class NordigenAccountInfoAPI {
         return basePostHttpRequest(linkUrl, bodyUrlEncodedString, "application/x-www-form-urlencoded");
     }
 
-    public String getAccountMetadata(String accountId) throws IOException, InterruptedException {
-        return baseGetHttpRequest(getBaseUrl() + ACCOUNT_URL + accountId + "/");
-    }
-
-    public String getAccountBalances(String accountId) throws IOException, InterruptedException {
-        return baseGetHttpRequest(getBaseUrl() + ACCOUNT_URL + accountId + "/balances/");
-    }
-
-    public String getAccountDetails(String accountId) throws IOException, InterruptedException {
-        return baseGetHttpRequest(getBaseUrl() + ACCOUNT_URL + accountId + "/details/");
-    }
-
-    public String getAccountTransactions(String accountId) throws IOException, InterruptedException {
-        return baseGetHttpRequest(getBaseUrl() + ACCOUNT_URL + accountId + "/transactions/");
-    }
-
     private String getBaseUrl() {
         return this.properties.getBaseUrl();
     }
