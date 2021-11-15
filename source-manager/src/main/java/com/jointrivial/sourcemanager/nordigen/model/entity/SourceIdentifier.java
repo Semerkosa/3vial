b59d19f8c021891231b2e3ffe8d0d20de3b1d83c
ANeshkova <anitowurth@abv.bg>
2021-11-15 12:29:18 +0200

    Add countries to DB using data.sql file
    #203

--- source-manager/src/main/java/com/jointrivial/sourcemanager/nordigen/model/entity/SourceIdentifier.java
+++ source-manager/src/main/java/com/jointrivial/sourcemanager/nordigen/model/entity/SourceIdentifier.java
@@ -6,7 +6,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "source_manager")
+@Table(name = "source_identifiers")
 public class SourceIdentifier {
 
     private String referenceId;
