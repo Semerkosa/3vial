7137d2dbe24f7da482d459e01eee3fc2b32a6e7a
Borislav Nenov <borislav.nenov.93@gmail.com>
2021-11-16 10:53:39 +0200

    Added the country and bank dropdowns to the profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,17 +1,35 @@
 import Button from '../components/Button';
 import SourceTypesDropDown from '../components/SourceTypesDropDown';
 import { useState } from 'react';
+import CountriesDropDown from '../components/CountriesDropDown';
+import BanksDropDown from '../components/BanksDropDown';
+
 const Profile = () => {
-    const [source, setSource] = useState('Bank');
+    const [source, setSource] = useState('');
+    const [country, setCountry] = useState('');
+    const [bankValue, setBankValue] = useState('');
+
     const addNewSourceHandler = (e) => {
         console.log(source);
-     };
+    };
+
     const selectSourceHandler = (sourceType) => {
         setSource(sourceType);
     };
+
+    const selectCountryHandler = (selectedCountry) => {
+        setCountry(selectedCountry);
+    }
+
+    const selectBankHandler = (selectedBank) => {
+        setBankValue(selectedBank);
+    }
+
     return (
         <>
             <SourceTypesDropDown onSelectSourceType={selectSourceHandler}></SourceTypesDropDown>
+            {source === 'Bank' && <CountriesDropDown onSelectCountry={selectCountryHandler} />}
+            {country !== '' && <BanksDropDown countryCode={country} onSelectBank={selectBankHandler} />}
             <Button type='submit' onClick={addNewSourceHandler}>Add source</Button>
         </>
     );
