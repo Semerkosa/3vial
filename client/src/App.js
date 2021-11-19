cda949918c66eb048695bbdfe55b7f532cc1b131
Stan Manilov <stanislav.manilov@gmail.com>
2021-11-19 16:22:17 +0200

    Add first tests for client

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import Profile from './pages/Profile';
 
 export const UserContext = React.createContext();
 function App() {
-  const [token, setToken] = useState('');
+  const [token, setToken] = useState('abc');
   let isLoggedIn = () => token !== '';
   return (
     <UserContext.Provider value={{ token, setToken }}>
