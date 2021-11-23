412ab9fd315c4cd02b5ae7f9122c055cbc619b8c
Semerkosa <60141925+Semerkosa@users.noreply.github.com>
2021-11-23 14:33:00 +0200

    adds comment in data.sql

--- source-manager/src/main/java/com/jointrivial/sourcemanager/nordigen/config/ApplicationBeanConfiguration.java
+++ source-manager/src/main/java/com/jointrivial/sourcemanager/nordigen/config/ApplicationBeanConfiguration.java
@@ -1,14 +1,11 @@
 package com.jointrivial.sourcemanager.nordigen.config;
 
-import com.google.gson.*;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.jointrivial.sourcemanager.nordigen.api.NordigenSourceLinkAPI;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.lang.reflect.Type;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @Configuration
 public class ApplicationBeanConfiguration {
