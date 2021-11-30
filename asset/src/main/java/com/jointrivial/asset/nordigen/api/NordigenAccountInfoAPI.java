68b28afa44301253e237b7a2aa440db0bfe2ba5b
Semerkosa <60141925+Semerkosa@users.noreply.github.com>
2021-11-30 13:47:50 +0200

    logic copy-pasted from source-manager
    API methords extracted in a base class and constants package added

--- asset/src/main/java/com/jointrivial/asset/nordigen/api/NordigenAccountInfoAPI.java
+++ asset/src/main/java/com/jointrivial/asset/nordigen/api/NordigenAccountInfoAPI.java
@@ -1,61 +1,24 @@
 package com.jointrivial.asset.nordigen.api;
 
-import com.jointrivial.asset.nordigen.config.ApplicationProperties;
-import org.springframework.beans.factory.annotation.Autowired;
-
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 
-public class NordigenAccountInfoAPI {
+import static com.jointrivial.asset.nordigen.constants.ApiURLs.ACCOUNT_URL;
 
-    private static final String ACCOUNT_URL = "/api/v2/accounts/";
-
-    @Autowired
-    private ApplicationProperties properties;
+public class NordigenAccountInfoAPI extends BaseNordigen{
 
     public String getAccountMetadata(String accountId) throws IOException, InterruptedException {
-        return baseGetHttpRequest(baseUrl() + ACCOUNT_URL + accountId + "/");
+        return baseGetHttpRequest(getBaseUrl() + ACCOUNT_URL + accountId + "/");
     }
 
     public String getAccountBalances(String accountId) throws IOException, InterruptedException {
-        return baseGetHttpRequest(baseUrl() + ACCOUNT_URL + accountId + "/balances/");
+        return baseGetHttpRequest(getBaseUrl() + ACCOUNT_URL + accountId + "/balances/");
     }
 
     public String getAccountDetails(String accountId) throws IOException, InterruptedException {
-        return baseGetHttpRequest(baseUrl() + ACCOUNT_URL + accountId + "/details/");
+        return baseGetHttpRequest(getBaseUrl() + ACCOUNT_URL + accountId + "/details/");
     }
 
     public String getAccountTransactions(String accountId) throws IOException, InterruptedException {
-        return baseGetHttpRequest(baseUrl() + ACCOUNT_URL + accountId + "/transactions/");
-    }
-
-    private String baseUrl() {
-        return this.properties.getBaseUrl();
-    }
-
-    private String token() {
-        return "Bearer " + this.properties.getAccessToken();
-    }
-
-    private String baseGetHttpRequest(String url) throws IOException, InterruptedException {
-        HttpClient client = HttpClient.newHttpClient();
-
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(url))
-                .header("Authorization", token())
-                .GET()
-                .build();
-
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-        if (response.statusCode() == 200) {
-            return response.body();
-        }
-
-        // TODO handle exception properly
-        return null;
+        return baseGetHttpRequest(getBaseUrl() + ACCOUNT_URL + accountId + "/transactions/");
     }
 }
---
+++ asset/src/main/java/com/jointrivial/asset/nordigen/models/services/NordigenAccessTokenServiceModel.java
@@ -0,0 +1,55 @@
+package com.jointrivial.asset.nordigen.models.services;
+
+import com.google.gson.annotations.Expose;
+
+public class NordigenAccessTokenServiceModel {
+
+    @Expose
+    private String access;
+    @Expose
+    private long access_expires;
+    @Expose
+    private String refresh;
+    @Expose
+    private long refresh_expires;
+
+
+    public NordigenAccessTokenServiceModel() {
+    }
+
+    public String getAccess() {
+        return access;
+    }
+
+    public NordigenAccessTokenServiceModel setAccess(String access) {
+        this.access = access;
+        return this;
+    }
+
+    public long getAccess_expires() {
+        return access_expires;
+    }
+
+    public NordigenAccessTokenServiceModel setAccess_expires(long access_expires) {
+        this.access_expires = access_expires;
+        return this;
+    }
+
+    public String getRefresh() {
+        return refresh;
+    }
+
+    public NordigenAccessTokenServiceModel setRefresh(String refresh) {
+        this.refresh = refresh;
+        return this;
+    }
+
+    public long getRefresh_expires() {
+        return refresh_expires;
+    }
+
+    public NordigenAccessTokenServiceModel setRefresh_expires(long refresh_expires) {
+        this.refresh_expires = refresh_expires;
+        return this;
+    }
+}
