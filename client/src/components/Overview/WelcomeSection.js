fa707d934ebfd73f3fd1183108f498cc5994fca3
ANeshkova <anitowurth@abv.bg>
2021-12-09 13:22:33 +0200

    Add a comment for more clarity
    #262

--- client/src/components/Overview/WelcomeSection.js
+++ client/src/components/Overview/WelcomeSection.js
@@ -2,6 +2,7 @@ const WelcomeSection = () => {
     return (
         <section className='c-welcome-section'>
             <div className='o-page-content-wrapper'>
+            {/* Later "Eric" will be replaced with a variable that contains the user's first name. */}
                 <h4>Good morning, Eric</h4>
                 <p>Have you added all your investments?</p>
                 <span>Add now</span>
