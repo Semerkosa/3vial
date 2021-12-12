64411867ca7133f8a5e74134f2701bb541f3e631
Borislav Nenov <borislav.nenov.93@gmail.com>
2021-12-12 16:06:09 +0200

    Added new bean and properties configuration

--- source-manager/src/main/java/com/jointrivial/sourcemanager/config/ApplicationBeanConfiguration.java
+++ source-manager/src/main/java/com/jointrivial/sourcemanager/config/ApplicationBeanConfiguration.java
@@ -1,8 +1,9 @@
-package com.jointrivial.sourcemanager.nordigen.config;
+package com.jointrivial.sourcemanager.config;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.jointrivial.sourcemanager.nordigen.api.NordigenSourceLinkAPI;
+import com.jointrivial.sourcemanager.yodlee.api.YodleeApi;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -26,6 +27,11 @@ public class ApplicationBeanConfiguration {
     @Bean
     public NordigenSourceLinkAPI nordigenAccountInfoAPI() {
         return new NordigenSourceLinkAPI();
+    }
+
+    @Bean
+    public YodleeApi yodleeApi() {
+        return new YodleeApi();
     }
 
 }
